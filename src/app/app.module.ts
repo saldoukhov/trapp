708f0aa8e437695faeeff915d225a748bc45dedc
Sergey Aldoukhov <saldoukhov@gmail.com>
2016-11-14 15:21:51 -0800

    Firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AngularFireModule, AuthMethods, AuthProviders} from 'angularfire2';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFRaD5M1zMPRKHQj5oshYLcyVa-cqq_Gw",
+  authDomain: "translation-5c988.firebaseapp.com",
+  databaseURL: "https://translation-5c988.firebaseio.com",
+  storageBucket: "translation-5c988.appspot.com"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +25,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
