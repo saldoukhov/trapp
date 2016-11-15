1a041b4f94aeb1dc1eb246869ed8c22162ccab17
Sergey Aldoukhov <saldoukhov@gmail.com>
2016-11-14 16:14:17 -0800

    Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AngularFireModule, AuthMethods, AuthProviders} from 'angularfire2';
+import {MaterialModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 
@@ -26,7 +27,8 @@ const firebaseAuthConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
